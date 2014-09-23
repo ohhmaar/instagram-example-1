b967bee5b169b539a8aa2fb2caa8d079c95bbb45
Omar Basrawi <omar@Omars-MacBook-Air.local>
2014-09-23 15:39:55 -0500

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Omar Basrawi on 9/23/14.
+//  Copyright (c) 2014 Omar Basrawi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photo Bombers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Omar Basrawi on 9/23/14.
+//  Copyright (c) 2014 Omar Basrawi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
